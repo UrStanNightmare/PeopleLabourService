40221060dee9baed4083ae94fad46f9d72a41643
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-08 05:07:35 +0700

    Конфигурация date time formatter-a

---
+++ src/main/java/ru/academicians/myhelper/config/DateTimeFormatConfiguration.java
@@ -0,0 +1,17 @@
+package ru.academicians.myhelper.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.academicians.myhelper.defaults.DefaultRequirements;
+
+@Configuration
+public class DateTimeFormatConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setTimeFormatter(DefaultRequirements.TIME_FORMATTER);
+        registrar.registerFormatters(registry);
+    }
+}
