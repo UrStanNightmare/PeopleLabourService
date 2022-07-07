c7bf715ea930dfc7661ac2d3c2c7c4c603fc036a
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-08 05:08:23 +0700

    Разнес в 2 контроллера для post и get запросов отдельно.

---
+++ src/main/java/ru/academicians/myhelper/controller/DefaultGetController.java
@@ -0,0 +1,50 @@
+package ru.academicians.myhelper.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.academicians.myhelper.model.DealInfoResponse;
+import ru.academicians.myhelper.service.DefaultDealsService;
+import ru.academicians.myhelper.service.DefaultUserService;
+
+import javax.validation.constraints.NotNull;
+
+@Validated
+@Api(description = "BaseApi")
+@RestController
+@RequestMapping("api")
+public class DefaultGetController {
+    private final DefaultUserService userService;
+    private final DefaultDealsService dealsService;
+
+    @Autowired
+    public DefaultGetController(DefaultUserService userService, DefaultDealsService defaultDealsService) {
+        this.userService = userService;
+        this.dealsService = defaultDealsService;
+    }
+
+    @ApiOperation(value = "Test availability")
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+
+
+        return new ResponseEntity<>("Greetings from back)", HttpStatus.I_AM_A_TEAPOT);
+    }
+
+    @ApiOperation(value = "Get deal information")
+    @GetMapping("/deal/{id}/info")
+    public ResponseEntity<DealInfoResponse> getDealInfo(@PathVariable(name = "id") @NotNull long id) {
+
+        DealInfoResponse dealInformation = dealsService.getDealInformation(id);
+
+        return new ResponseEntity<>(dealInformation, HttpStatus.OK);
+    }
+
+}
--- src/main/java/ru/academicians/myhelper/controller/DefaultPostController.java
+++ src/main/java/ru/academicians/myhelper/controller/DefaultPostController.java
@@ -6,11 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.academicians.myhelper.exception.ItemNotFoundException;
+import ru.academicians.myhelper.model.AddServiceRequest;
+import ru.academicians.myhelper.model.SubscribeRequest;
+import ru.academicians.myhelper.repository.model.Deal;
+import ru.academicians.myhelper.repository.model.User;
+import ru.academicians.myhelper.service.DefaultDealsService;
 import ru.academicians.myhelper.service.DefaultUserService;
 import ru.academicians.myhelper.model.AddPersonRequest;
 import ru.academicians.myhelper.model.OperationResultResponse;
 
 import javax.validation.Valid;
+
+import static ru.academicians.myhelper.defaults.DefaultKeys.*;
 
 @Api(description = "BaseApi")
 @RestController
@@ -18,25 +26,69 @@ import javax.validation.Valid;
 public class DefaultPostController {
 
     private final DefaultUserService userService;
+    private final DefaultDealsService dealsService;
 
     @Autowired
-    public DefaultPostController(DefaultUserService userService) {
+    public DefaultPostController(DefaultUserService userService, DefaultDealsService dealsService) {
         this.userService = userService;
-    }
-
-    @ApiOperation(value = "Test availability")
-    @GetMapping("/test")
-    public ResponseEntity<String> test(){
-
-        return new ResponseEntity<>("Greetings from back)", HttpStatus.I_AM_A_TEAPOT);
+        this.dealsService = dealsService;
     }
 
     @ApiOperation(value = "An attempt to add user to db")
-    @PostMapping("/add")
-    public ResponseEntity<OperationResultResponse> addUser(@Valid @RequestBody AddPersonRequest request){
+    @PostMapping("/add/user")
+    public ResponseEntity<OperationResultResponse> addUser(@Valid @RequestBody AddPersonRequest request) {
 
-        String result = userService.createNewUser(request);
+        long id = userService.createNewUser(request);
 
-        return new ResponseEntity<>(new OperationResultResponse("Add", result), HttpStatus.CREATED);
+
+        return new ResponseEntity<>(new OperationResultResponse("Add", ((Long) id).toString()), HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "An attempt to add service to db")
+    @PostMapping("/add/deal")
+    public ResponseEntity<OperationResultResponse> addUser(@Valid @RequestBody AddServiceRequest request) {
+
+        User user = userService.findUserById(request.getOwnerId());
+
+        if (user == null) {
+            throw new ItemNotFoundException(USER_NOT_FOUND_STRING);
+        }
+
+        long id = dealsService.createNewDeal(request, user);
+
+        return new ResponseEntity<>(new OperationResultResponse("Add", ((Long) id).toString()), HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "An attempt to subscribe user to deal")
+    @PostMapping("/subscribe/deal")
+    public ResponseEntity<OperationResultResponse> subscribeUser(@Valid @RequestBody SubscribeRequest request) {
+
+        long subscriberId = request.getSubscriberId();
+        long dealId = request.getDealId();
+
+        User user = userService.findUserById(subscriberId);
+
+        if (user == null) {
+            throw new ItemNotFoundException(USER_NOT_FOUND_STRING);
+        }
+        Deal deal = dealsService.findDealById(dealId);
+
+        if (deal == null) {
+            throw new ItemNotFoundException(DEAL_NOT_FOUND_STRING);
+        }
+
+        if (deal.getOwner().getId() == subscriberId) {
+
+            throw new IllegalArgumentException(USER_CANT_SUBSCRIBE_SELF_STRING);
+
+        }
+
+        long id = dealsService.subscribeUserToDealAndSave(user, deal);
+
+        return new ResponseEntity<>(
+                new OperationResultResponse(
+                        "Subscribe",
+                        subscriberId + " to " + ((Long) id).toString()),
+                HttpStatus.CREATED);
     }
 }
