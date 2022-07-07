84a779f332b08c1e17427562b62b31957269c59b
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-08 05:10:48 +0700

    Репозитории для работы с энтити.

--- src/main/java/ru/academicians/myhelper/repository/DefaultUserRepository.java
+++ src/main/java/ru/academicians/myhelper/repository/DefaultUserRepository.java
@@ -1,15 +1,15 @@
 package ru.academicians.myhelper.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.academicians.myhelper.repository.model.User;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Repository
+public interface DefaultUserRepository extends JpaRepository<User, Long> {
 
-    List<User> findAll();
-
-    User getUserById(Long id);
+    User getUserById(long id);
 
     User getUserByLastNameAndFirstNameAndPatronymic(String lastName, String firstName, String patronymic);
 }
