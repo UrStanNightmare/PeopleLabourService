791f21f7d3748230ba5feb72b359c0ec5e746c9a
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-08 18:00:01 +0700

    Создал мапперы для создания моделей из данных в бд.

---
+++ src/main/java/ru/academicians/myhelper/repository/mapper/SubscriptionRecordRowMapper.java
@@ -0,0 +1,21 @@
+package ru.academicians.myhelper.repository.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.academicians.myhelper.model.SubscriptionRecord;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class SubscriptionRecordRowMapper implements RowMapper<SubscriptionRecord> {
+    @Override
+    public SubscriptionRecord mapRow(ResultSet rs, int i) throws SQLException {
+        SubscriptionRecord result = new SubscriptionRecord();
+
+        result.setDealId(rs.getLong("deal_id"));
+        result.setSubscriberId(rs.getLong("subscribers_id"));
+
+        return result;
+    }
+}
