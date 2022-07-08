b680d3bd093a82497c19a4fbb0a0abd32ccefaee
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-08 18:00:24 +0700

    Убрал гибернейт.

--- src/main/java/ru/academicians/myhelper/repository/model/User.java
+++ src/main/java/ru/academicians/myhelper/repository/model/User.java
@@ -1,47 +1,23 @@
 package ru.academicians.myhelper.repository.model;
 
-import ru.academicians.myhelper.defaults.DefaultRequirements;
-
-import javax.persistence.*;
-
-import java.util.LinkedHashSet;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
-import java.util.Set;
 
-import static ru.academicians.myhelper.defaults.DefaultKeys.*;
-
-@Entity
-@Table(name = USERS_TABLE_NAME)
 public class User {
-
-    @Id
-    @SequenceGenerator(name = "userIdSeq",
-            sequenceName = "users_id_sequence",
-            allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "userIdSeq")
     private Long id;
-    @Column(name = LAST_NAME_KEY,
-            nullable = false,
-            length = DefaultRequirements.MAX_NAME_LENGTH)
+
+    @NotNull
+    @NotBlank
     private String lastName;
 
-    @Column(name = FIRST_NAME_KEY,
-            nullable = false,
-            length = DefaultRequirements.MAX_NAME_LENGTH)
+    @NotNull
+    @NotBlank
     private String firstName;
 
-    @Column(name = PATRONYMIC_KEY,
-            nullable = true,
-            length = DefaultRequirements.MAX_NAME_LENGTH)
+    @NotNull
+    @NotBlank
     private String patronymic;
-
-    @OneToMany(mappedBy = "owner",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true,
-            fetch = FetchType.LAZY)
-    private Set<Deal> deals = new LinkedHashSet<>();
-
 
     public User() {
     }
@@ -53,19 +29,12 @@ public class User {
         this.patronymic = patronymic;
     }
 
-    public User(String lastName, String firstName, String patronymic, Set<Deal> deals) {
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.patronymic = patronymic;
-        this.deals = deals;
-    }
 
-    public User(Long id, String lastName, String firstName, String patronymic, Set<Deal> deals) {
+    public User(Long id, String lastName, String firstName, String patronymic) {
         this.id = id;
         this.lastName = lastName;
         this.firstName = firstName;
         this.patronymic = patronymic;
-        this.deals = deals;
     }
 
     public Long getId() {
@@ -100,24 +69,16 @@ public class User {
         this.patronymic = patronymic;
     }
 
-    public Set<Deal> getServices() {
-        return deals;
-    }
-
-    public void setServices(Set<Deal> deals) {
-        this.deals = deals;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id.equals(user.id) && lastName.equals(user.lastName) && firstName.equals(user.firstName) && Objects.equals(patronymic, user.patronymic) && Objects.equals(deals, user.deals);
+        return id.equals(user.id) && lastName.equals(user.lastName) && firstName.equals(user.firstName) && Objects.equals(patronymic, user.patronymic);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, lastName, firstName, patronymic, deals);
+        return Objects.hash(id, lastName, firstName, patronymic);
     }
 }
