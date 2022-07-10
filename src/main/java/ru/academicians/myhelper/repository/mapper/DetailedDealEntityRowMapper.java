0d5438f94ba68702ceef131de25dd723941bf807
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-10 19:48:37 +0700

    Добавил маппер для преобразования детальной информации об услуге из бд.

---
+++ src/main/java/ru/academicians/myhelper/repository/mapper/DetailedDealEntityRowMapper.java
@@ -0,0 +1,37 @@
+package ru.academicians.myhelper.repository.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.academicians.myhelper.repository.model.Deal;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Component
+public class DetailedDealEntityRowMapper implements RowMapper<Deal> {
+    @Override
+    public Deal mapRow(ResultSet rs, int i) throws SQLException {
+        Deal result = new Deal();
+        result.setId(rs.getLong("id"));
+        result.setCity(rs.getString("service_city"));
+
+        Instant service_time = rs.getTimestamp("service_date").toInstant();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(service_time, ZoneOffset.UTC);
+        result.setDate(localDateTime);
+        result.setDescription(rs.getString("service_desc"));
+        result.setName(rs.getString("service_name"));
+        result.setPrice(rs.getBigDecimal("service_price").toBigInteger());
+        result.setOwner(rs.getLong("owner_id"));
+        String subscribersString = rs.getString("subscribers");
+        String[] split = subscribersString.split(",");
+        for (String s : split) {
+            long subscriberId = Long.parseLong(s);
+            result.addSubscriberId(subscriberId);
+        }
+
+        return result;
+    }
+}
