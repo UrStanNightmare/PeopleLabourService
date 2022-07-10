ac9de4a7df1c6bbeb3fb004c751d16acaf9fda81
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-10 19:51:14 +0700

    Обновил интерфейс сервиса для сделок.

--- src/main/java/ru/academicians/myhelper/service/DefaultDealsService.java
+++ src/main/java/ru/academicians/myhelper/service/DefaultDealsService.java
@@ -1,9 +1,12 @@
 package ru.academicians.myhelper.service;
 
 import ru.academicians.myhelper.model.AddServiceRequest;
+import ru.academicians.myhelper.model.AllDealsResponse;
 import ru.academicians.myhelper.model.DealInfoResponse;
 import ru.academicians.myhelper.repository.model.Deal;
 import ru.academicians.myhelper.repository.model.User;
+
+import java.util.List;
 
 public interface DefaultDealsService {
     long createNewDeal(AddServiceRequest data, User user);
@@ -13,4 +16,10 @@ public interface DefaultDealsService {
     String addSubscription(long dealId, long subscriberId);
 
     DealInfoResponse getDealInformation(long id);
+
+    List<Deal> findDealsByOwnerId(long id);
+
+    boolean isSubscriptionExists(long dealId, long subscriberId);
+
+    AllDealsResponse getAllDealsInfo();
 }
