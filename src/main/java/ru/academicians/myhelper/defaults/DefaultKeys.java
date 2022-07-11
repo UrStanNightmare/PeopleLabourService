60b572b75982b9c83e61cf8a91ee681aa722b27d
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-12 03:36:57 +0700

    Добавил несколько констант.

--- src/main/java/ru/academicians/myhelper/defaults/DefaultKeys.java
+++ src/main/java/ru/academicians/myhelper/defaults/DefaultKeys.java
@@ -7,9 +7,43 @@ public class DefaultKeys {
     public static final String USER_CANT_SUBSCRIBE_SELF_STRING = "User can't subscribe to his own deal!";
     public static final String USER_ALREADY_SUBSCRIBED_STRING = "User already subscribed!";
     public static final String DEAL_NOT_FOUND_STRING = "Deal not found!";
-    public static final String USERNAME_NOT_FOUND = "Authentication failed! User not found.";
+    public static final String AUTHENTICATION_FAILED = "Authentication failed!";
 
 
+    public static final String PRIVATE_KEY = "MIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQC3o7qsi0b9YoIO\n" +
+            "BjagjYBaPcdtBISncQyNvWODgpoHPxq+z9eQHT8x5MkhAb10nV3+Rz92m2tGGF1v\n" +
+            "5IC7aL4S0X5MhVcUAURxjAx/n362SJ0EUoVqmzkvIHnXYm4UHixoZjNkZcyVaBhI\n" +
+            "Qu/uFOnRbgc0+UoGm1GwRe7EyNpsrXBGENdKrgHQsHX0OmIZ21zp8Xg0nk/zlanz\n" +
+            "9bT7tsfL79KTfg9sLDJcHx5QEA6s4NwDHBqfVJ4Cn+GrMJ3UMLhkRgGL3HKqCI2H\n" +
+            "hZny82csXzh/9gmkCM59o4dXaFtIj4k7hCkYPDcUnF9WILVC7qGhgrB7fKqjHV5b\n" +
+            "TanU/dPFAgMBAAECgf9pRLH9ttk65v0mjOlDn9mz4TYwiCaC1YsctTQWbwdcoA/0\n" +
+            "0bAALMPNFhk1konID22SFcWLeAxGYjEtsw8YXzKWFwEPXGBOV2ukq2N+xS8dzwIz\n" +
+            "OBrCwbhGC5fMn9/6KFHMhslvA+wCuWkyAy0GjXZ7V1dKRpzorKBnK69znq+XG9wh\n" +
+            "tUYUAfkIehA3ItDTPg8gM42fFJaWL9hGY9+u6w4ghTyF2e0V05nlAE8UD96NbkYP\n" +
+            "cHnW8JxH/XtlfaJ8beiz8d5KY20zOY9Flb16aqTaeWeWambccVk0AMdH3AqGSoNy\n" +
+            "dxd6u8KsCsJ4TrA3Rubns8PHZjTEQbqp5SdHsYECgYEAzqgqzom8HcV1Iz3eoAmF\n" +
+            "IkwHAbNp6Jd2UvMaDUX5x/P/c812ucMZv6nwzzaBorpZCy56KrFy/PM/dUsAi02U\n" +
+            "Zf4coAc/G2LC+8wBUsxcAsIrkJZLPXC3rO6OhEe/dVAxSYcLN7N2L9q+ACRJUVeH\n" +
+            "7Y5mgvBvt0YQy1n6nAWZhUUCgYEA43yimerB1EXCWcqCFP5aysPW9ijgTcYOA8WS\n" +
+            "kHe8lVo0pHn91D6nteiETha6j4JcgejCJYVcQ8gdPQlyvmVapsZct+T8CQ8vT3Le\n" +
+            "iHVBQ6vZXapOy7e7LYQAi/FTuv8z4RaivgdQS2zNe4V1bJnoddQgrctPRDEQ8VhU\n" +
+            "rm5nvIECgYEApDEMwcjBQ84swXFYvkKSGqvZLUrFLN2aT1GDM2STjoCYSAPI/tK6\n" +
+            "VIPLJneiDA6OzMmCmwUd3UfO9Xw/1G4xJ8/8IlFfnHyder2dl5Fy0WZAgsJGyWwT\n" +
+            "Izzu0r7GqYirgEXhbcISdvcO2sH51rz67I2leQdbW3w5v7ef2/K/BRkCgYAu2KPD\n" +
+            "foyb4iG7y/At0pNv3hSObKyEV5zbOfyMs5uqRgFBPb3/z0dKNq/IRPX2R6o1NnGE\n" +
+            "uInfi34565AtlUGtxnuhgw5D71SWz9jz0PG3tdP1bfT8DsIFBYH3T6Mmhxdv6vFo\n" +
+            "oKjwX1+RaFJeIvO0EBMKtyuDp3cNvFrz8Nh8AQKBgCywUCYMmn2JyqrRVG03wEUD\n" +
+            "qJAO7UJdFTWRbQeylE/fFXqLz2Qr8K5i+k1YNsJqLqrcT/w6V1sW7+u9O+7RMHt6\n" +
+            "2T+mp9TW1/wx0WmiYcS+BJE3K5qlQSdDCPFxon7i3dL7LoiO6O9OSQiR2d+k6ON8\n" +
+            "gDcKrOhDiWVoRF6GonNY";
+
+    public static String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt6O6rItG/WKCDgY2oI2A\n" +
+            "Wj3HbQSEp3EMjb1jg4KaBz8avs/XkB0/MeTJIQG9dJ1d/kc/dptrRhhdb+SAu2i+\n" +
+            "EtF+TIVXFAFEcYwMf59+tkidBFKFaps5LyB512JuFB4saGYzZGXMlWgYSELv7hTp\n" +
+            "0W4HNPlKBptRsEXuxMjabK1wRhDXSq4B0LB19DpiGdtc6fF4NJ5P85Wp8/W0+7bH\n" +
+            "y+/Sk34PbCwyXB8eUBAOrODcAxwan1SeAp/hqzCd1DC4ZEYBi9xyqgiNh4WZ8vNn\n" +
+            "LF84f/YJpAjOfaOHV2hbSI+JO4QpGDw3FJxfViC1Qu6hoYKwe3yqox1eW02p1P3T\n" +
+            "xQIDAQAB";
     private DefaultKeys() {
     }
 }
