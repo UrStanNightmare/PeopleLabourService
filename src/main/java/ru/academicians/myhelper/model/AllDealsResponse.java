486e38236b68c53252d4a3a69f8f53be6d4f9961
Anton Oshchepkov <antonoschepkov27@gmail.com>
2022-07-13 17:10:47 +0700

    Добавление возможности вставить фильтр.

--- src/main/java/ru/academicians/myhelper/model/AllDealsResponse.java
+++ src/main/java/ru/academicians/myhelper/model/AllDealsResponse.java
@@ -1,10 +1,13 @@
 package ru.academicians.myhelper.model;
 
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class AllDealsResponse {
     private Set<DealInfoResponse> deals;
+
+    private Map<String, Object> filter;
 
     public AllDealsResponse() {
         this.deals = new LinkedHashSet<>();
@@ -24,5 +27,13 @@ public class AllDealsResponse {
 
     public void addDeal(DealInfoResponse deal){
         this.deals.add(deal);
+    }
+
+    public Map<String, Object> getFilter() {
+        return filter;
+    }
+
+    public void setFilter(Map<String, Object> filter) {
+        this.filter = filter;
     }
 }
